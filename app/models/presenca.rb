70c60fae65f7d1e8f27e2a4852640e2385dfc520
pessoaittalo <francisco.itallo@dcx.ufpb.br>
2026-09-24 10:58:51 -0300

    .

--- app/models/presenca.rb
+++ app/models/presenca.rb
@@ -9,4 +9,13 @@ class Presenca < ApplicationRecord
               scope: :atividade_id,
               message: "já possui presença nesta atividade"
             }
+
+  # Avisa as telas abertas quando uma nova presença for registrada
+  after_create_commit :atualizar_chamada
+
+  private
+
+  def atualizar_chamada
+    broadcast_refresh_to atividade
+  end
 end
